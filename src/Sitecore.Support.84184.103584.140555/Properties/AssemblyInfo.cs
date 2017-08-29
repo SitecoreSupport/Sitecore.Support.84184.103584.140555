fb72c528766f5285ecb2de8d05f142458618ff1c
Eugene Oparyshev <eop@sitecore.net>
2017-08-29 09:54:50 +0300

    Github Patch

---
+++ src/Sitecore.Support.84184.103584.140555/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.84184.103584.140555")]
+[assembly: AssemblyProduct("Sitecore.Support.84184.103584.140555")]
+[assembly: ComVisible(false)]
